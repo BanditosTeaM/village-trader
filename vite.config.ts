806c26aabd0fc745c6275780ac6a495afee6ae8d
BanditosTM <banditostm123@gmail.com>
2024-05-11 21:44:44 +0300

    feat: adding alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@providers": path.resolve(__dirname, "./src/providers"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@temp": path.resolve(__dirname, "./src/temp"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+    },
+  },
+});
