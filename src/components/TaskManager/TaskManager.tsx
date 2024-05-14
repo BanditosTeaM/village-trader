8e3cc9546e585c8a21afd79bce18c78a8cae7200
BanditosTM <banditostm123@gmail.com>
2024-05-14 22:30:27 +0300

    feat: creating component for tasks

---
+++ src/components/TaskManager/TaskManager.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+
+interface TaskManagerProps {
+  products: { id: number; name: string; quantity: number }[];
+  onChangeProducts: (id: number, newQuantity: number, paymant: number) => void;
+}
+
+interface Task {
+  id: number;
+  name: string;
+  quantity: number;
+  payment: number;
+}
+
+const generateRandomTask = (
+  products: { id: number; name: string; quantity: number }[],
+  count: number
+) => {
+  const newTask = [];
+
+  if (products.length === 0) {
+    return [];
+  }
+
+  for (let i = 0; i < count; i++) {
+    const randomIndex = Math.floor(Math.random() * products.length);
+    const randomProduct = products[randomIndex];
+    const randomQuantity = Math.floor(Math.random() * 5) + 1;
+
+    newTask.push({
+      id: randomProduct.id,
+      name: randomProduct.name,
+      quantity: randomQuantity,
+      payment: randomQuantity * 30,
+    });
+  }
+
+  return newTask;
+};
+
+export const TaskManager = ({
+  products,
+  onChangeProducts,
+}: TaskManagerProps) => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const count = 3;
+
+  useEffect(() => {
+    generateRandomTask(products, count);
+
+    const intervalId = setInterval(() => {
+      generateRandomTask(products, count);
+    }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handleButtonClick = (
+    index: number,
+    id: number,
+    quantity: number,
+    paymant: number
+  ) => {
+    onChangeProducts(id, quantity, paymant);
+    setTasks((prevTasks) => prevTasks.filter((_, i) => i !== index));
+  };
+
+  return (
+    <section>
+      <h2>Список задач</h2>
+      {tasks.length > 0 ? (
+        tasks.map((task, index) => (
+          <div key={index}>
+            Необходимо {task.quantity} шт. {task.name} <br />
+            Оплата: {task.payment} <br />
+            {index}
+            <button
+              onClick={() =>
+                handleButtonClick(index, task.id, task.quantity, task.payment)
+              }
+            >
+              Отправить
+            </button>
+          </div>
+        ))
+      ) : (
+        <div>Нет доступных продуктов для задания</div>
+      )}
+    </section>
+  );
+};
