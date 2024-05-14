24c34d6b6f995ab1bccf594d79f86df752148c23
BanditosTM <banditostm123@gmail.com>
2024-05-14 22:22:59 +0300

    feat: adding a function for buying and selling a product

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,73 @@
+import { useEffect, useState } from "react";
 import { Cities } from "../Cities";
+import { Storage } from "../Storage";
+import { TaskManager } from "../TaskManager";
+import productsData from "@api/products.json";
 import "./App.css";
+
 export const App = () => {
+  const [day, setDay] = useState(1);
+  const [money, setMoney] = useState(500);
+  const [products, setProducts] = useState(productsData);
+
+  const buyProducts = (id: number, newQuantity: number, price: number) => {
+    const totalCost = newQuantity * price;
+
+    if (money >= totalCost) {
+      setProducts((prevProducts) =>
+        prevProducts.map((product) =>
+          product.id === id
+            ? { ...product, quantity: product.quantity + newQuantity }
+            : product
+        )
+      );
+
+      setMoney((prevMoney) => prevMoney - totalCost);
+      console.log("Покупка совершена!");
+    } else {
+      console.log("Недостаточно денег");
+    }
+  };
+
+  const sellProducts = (id: number, quantity: number, payment: number) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((product) => {
+        if (product.id === id) {
+          if (product.quantity >= quantity) {
+            setMoney((prevMoney) => prevMoney + payment);
+            console.log("Продажа совершена!");
+
+            return { ...product, quantity: product.quantity - quantity };
+          } else {
+            console.log("Недостаточно товара для продажи!");
+          }
+        }
+        return product;
+      })
+    );
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDay((prevDay) => prevDay + 1);
+    }, 25000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <>
       <div className="container">
         <header>Деревенский трейдер</header>
-        <Cities />
+        <Cities products={products} onChangeProducts={buyProducts} />
+        <div>
+          <Storage products={products} />
+          <div>
+            <div>День: {day}</div>
+            <div>Деньги: {money}</div>
+          </div>
+          <TaskManager products={products} onChangeProducts={sellProducts} />
+        </div>
       </div>
     </>
   );
