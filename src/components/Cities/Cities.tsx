ae9db137686c33f98ffdceef913a303152a9c5bd
BanditosTM <banditostm123@gmail.com>
2025-05-30 01:37:20 +0300

    style: adding styles for Cities component

--- src/components/Cities/Cities.tsx
+++ src/components/Cities/Cities.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export const Cities = () => {
   return (
     <>
-      <div>
+      <div className="city-list">
         {citiesData.map((city) => (
           <Link key={city.id} to={`/city/${city.id}`}>
             {city.name}
